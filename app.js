2ecd3b7cc3b0ddbfe16128904beb318186123f72
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-03-04 19:10:07 +0100

    feat (bo) refonte entière et affichage du prix et de la disponibilité de chaque articles

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
+'use strict';
 const puppeteer = require("puppeteer")
 const mysql = require('./bdd');
-const func = require('./func');
+const Copbot = require('./func');
 
-const getData = async () => {
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
-  
-    await page.goto("https://www.nike.com/fr/launch/?s=upcoming")
-    await page.waitFor(1000)
-      const result = await page.evaluate(() => {
-          let i = 0;
-          let nameFuturStock = [];
-          let linkFuturStock = [];
-          while (i < document.getElementsByClassName('pb2-sm va-sm-t ncss-col-sm-12').length){
-              nameFuturStock.push(document.getElementsByClassName('pb2-sm va-sm-t ncss-col-sm-12')[i].lastChild.innerText);
-              i++;
-          }
-          return { nameFuturStock };
-        })
-    console.log(result.nameFuturStock);
-    browser.close();
-  }
-  
-  getData();
+(async () => {
+    try {
+        const browser = await puppeteer.launch({ headless: false })
+
+        //const page = await browser.newPage()
+
+        const copbot = await Copbot(browser);
+        copbot.importUrlFollowing();
+
+ } catch (error) {
+    console.log(error);
+}
+    
+})();
--- func.js
+++ func.js
@@ -1,5 +1,57 @@
+'use strict';
 const mysql = require('./bdd');
+const UserAgent = require('user-agents');
 
-let timestamp = Math.round(new Date().getTime() / 1000);
+module.exports = async (browser) => {
+    let page;
 
-exports.timestamp = timestamp;
+    const sleep = (ms, dev = 1) => {
+        const msWithDev = ((Math.random() * dev) + 1) * ms;
+        console.log('Sleeping', msWithDev / 1000, 'sec');
+        return new Promise(resolve => setTimeout(resolve, msWithDev));
+    };
+
+    async function importInBddPage(link) {
+        let i = 0;
+        while (i < link.length){
+            await page.goto(link[i]);
+            const result = await page.evaluate(() => {
+                let price = document.getElementsByClassName('ncss-brand pb6-sm fs14-sm fs16-md')[0].innerText;
+                let date = document.getElementsByClassName('ncss-brand pb6-sm fs14-sm fs16-md')[1].innerText;
+                  return { price, date}
+                });
+                console.log('-----------');
+                console.log(result.price);
+                console.log(result.date);
+                console.log('-----------');
+            i++;
+        }
+    }
+
+    async function importUrlFollowing() {
+        await page.goto("https://www.nike.com/fr/launch/?s=upcoming")
+        await page.waitFor(1000)
+
+        const result = await page.evaluate(() => {
+        let i = 0;
+          let nameFuturStock = [];
+          let linkFuturStock = [];
+          while (i < document.getElementsByClassName('pb2-sm va-sm-t ncss-col-sm-12').length){
+              let link = document.getElementsByClassName('pb2-sm va-sm-t ncss-col-sm-12')[i].lastChild.getElementsByClassName('card-link d-sm-b')[0].href;
+                linkFuturStock.push(link);
+                //nameFuturStock.push(document.getElementsByClassName('pb2-sm va-sm-t ncss-col-sm-12')[i].lastChild.innerText);
+              i++;
+          }
+          return linkFuturStock
+        });
+          importInBddPage(result);
+    }
+
+  page = await browser.newPage();
+  const userAgent = new UserAgent();
+  await page.setUserAgent(userAgent.toString());
+  return {
+    sleep,
+    importUrlFollowing,
+  };
+};
